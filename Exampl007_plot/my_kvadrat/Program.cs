7300382129a73b2aebba81322ad077f685452888
AxelMaksel <am3mail@gmail.com>
2022-10-12 22:03:46 +0700

    ALL

---
+++ Exampl007_plot/my_kvadrat/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+int count = 0;
+while (count <10)
+{
+    int x = (new Random().Next(0,60));
+    int y = (new Random().Next(0,20));
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    System.Threading.Thread.Sleep(15);
+    count ++;
+}
+Console.SetCursorPosition(22, 22);
+count = 0;
+int yy = 1;
+while (yy < 20)
+{
+    while (count <30)
+    {
+        Console.SetCursorPosition(count, yy);
+        Console.WriteLine("  ");
+        count ++;
+        Console.SetCursorPosition(count, yy);
+        Console.WriteLine("OO");
+        System.Threading.Thread.Sleep(20);
+    }
+    Console.SetCursorPosition(count, yy);
+    Console.WriteLine("  ");
+    count = 0;
+    yy ++;
+}
+Console.SetCursorPosition(22, 22);
