f8440a630bd03f0edd9fac73419084290177f1a8
AxelMaksel <am3mail@gmail.com>
2022-10-12 10:13:50 +0700

    ур 5й Маша

---
+++ Exampl005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ваше имя:");
+string username = Console.ReadLine();
+Console.WriteLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет Машка");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
